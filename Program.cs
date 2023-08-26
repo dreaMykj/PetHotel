2b0985b6c0db91c045ae495132da5c2a589a5439
dreaMykj <gabrielgreghi2018@gmail.com>
2023-08-25 22:42:08 -0300

    Concluído

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("+=========================================================+");
+Console.WriteLine("|                 Pet Hotel                               |");
+Console.WriteLine("+=========================================================+");
+Console.WriteLine("| Espécie: ............CÃO | Raça: ............PITBULL    |");
+Console.WriteLine("+=========================================================+");
+Console.WriteLine("| Atende pela alcunha de: ...........................ZEUS |");
+Console.WriteLine("| Idade: 03 ano(s) | Pelagem/cor: ...............PRETO    |");
+Console.WriteLine("+=========================================================+");
